03ff200b59e3e485669bfd59d7f2527ae16d3ffe
Davenury <57959794+Davenury@users.noreply.github.com>
2020-04-18 13:32:07 +0200

    Add files via upload

--- recipes/models.py
+++ recipes/models.py
@@ -1,10 +1,15 @@
 from django.db import models
-
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
 
 
 class Recipe(models.Model):
-    difficulty = models.IntegerField(default=1)
+    class Difficulty(models.IntegerChoices):
+        VERY_EASY = 1
+        EASY = 2
+        MEDIUM = 3
+        HARD = 4
+        VERY_HARD = 5
+    difficulty = models.IntegerField(choices = Difficulty.choices, default = Difficulty.MEDIUM)
     name = models.CharField(max_length=100)
     image = models.CharField(max_length=200) #link do obrazka
 
@@ -28,16 +33,27 @@ class Ingredient(models.Model):
 
 
 class IngredientInstance(models.Model):
+    class Measurement(models.TextChoices):
+        PIECE = 'PSC'
+        CUP = 'C'
+        ML = 'ML'
+        L = 'L'
+        G = 'G'
+        KG = 'KG'
+        SPOON = 'SP'
+        TEASPOON = 'TSP'
+        PINCH = 'P'
+        CLOVE = 'CLV'
     ingredient = models.ForeignKey(Ingredient, on_delete=models.DO_NOTHING)
     how_much = models.FloatField(default=0)
-    how_much_of_what = models.CharField(max_length=10, default="")
+    how_much_of_what = models.CharField(choices = Measurement.choices, max_length = 5)
     recipe = models.ManyToManyField(Recipe)
 
     def __str__(self):
         return self.ingredient.name + " " + str(self.how_much) + " " + self.how_much_of_what
 
 
-class Steps(models.Model):
+class Step(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.DO_NOTHING)
     number = models.IntegerField()
     text = models.CharField(max_length=200)
