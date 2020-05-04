7698033c67987f26d6e77daca56ed98b11bf85e4
Davenury <57959794+Davenury@users.noreply.github.com>
2020-05-04 12:36:11 +0200

    Add files via upload

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import get_object_or_404,render
+from django.shortcuts import get_object_or_404,render,redirect
 from django.template import loader
 from django.http import HttpResponse
 from .models import Recipe, Category
 from django.http import Http404
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+
+def home(request):
+    return render(request, 'Registration/home.html')
 
 
 def index(request):
@@ -19,11 +25,14 @@ def detail(request, recipe_id):
         'name' : recipe.name,
         'difficulty' : recipe.difficulty,
         'image' : recipe.image,
-        'steps' : recipe.steps_set.all().order_by('number'),
+        'step' : recipe.step_set.all().order_by('number'),
         'categories' : recipe.category_set.all(),
+        'range_for_stars': range(recipe.difficulty),
+        'range_for_rest_stars': range(recipe.difficulty+1, 6),
     }
     return render(request, 'recipes/detail.html', context)
-    
+
+
 def categories(request):
     categories = Category.objects.all()
     context = {
@@ -31,9 +40,26 @@ def categories(request):
     }
     return render(request, 'categories/index.html', context)
 
+
 def category(request, category_id):
     category = get_object_or_404(Category, pk = category_id)
     context = {
         'category' : category,
-        'recipes' : category.recipe_set.all()
+        'recipes' : category.recipe.all()
     }
+    return render(request, 'categories/detail.html', context)
+
+
+def signup(request):  #przesada przerzucać do nowej aplikacji
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'Registration/signup.html', {'form': form})
