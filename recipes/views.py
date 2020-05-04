e28526d62959531d6217a2b6c0c5a4169f86a894
Davenury <57959794+Davenury@users.noreply.github.com>
2020-05-04 14:33:17 +0200

    Add files via upload

--- recipes/views.py
+++ recipes/views.py
@@ -6,7 +6,6 @@ from django.http import Http404
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
-
 def home(request):
     return render(request, 'Registration/home.html')
 
@@ -26,6 +25,7 @@ def detail(request, recipe_id):
         'difficulty' : recipe.difficulty,
         'image' : recipe.image,
         'step' : recipe.step_set.all().order_by('number'),
+        'ingredients' : recipe.ingredientinstance_set.all(),
         'categories' : recipe.category_set.all(),
         'range_for_stars': range(recipe.difficulty),
         'range_for_rest_stars': range(recipe.difficulty+1, 6),
@@ -50,7 +50,7 @@ def category(request, category_id):
     return render(request, 'categories/detail.html', context)
 
 
-def signup(request):
+def signup(request):  #przesada przerzucać do nowej aplikacji
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -63,3 +63,20 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'Registration/signup.html', {'form': form})
+
+
+def search(request):
+    query = request.GET.get('q')
+    query = '%' + query + '%'
+    recipes = Recipe.objects.raw(
+        """SELECT DISTINCT recipes_recipe.id, recipes_recipe.difficulty, recipes_recipe.name, recipes_recipe.image 
+        FROM recipes_recipe 
+        LEFT OUTER JOIN recipes_ingredientinstance_recipe ON (recipes_recipe.id = recipes_ingredientinstance_recipe.recipe_id) 
+        INNER JOIN recipes_ingredientinstance ON recipes_ingredientinstance_recipe.ingredientinstance_id =  recipes_ingredientinstance.id
+        INNER JOIN recipes_ingredient ON recipes_ingredientinstance.ingredient_id = recipes_ingredient.id
+        WHERE (UPPER(recipes_recipe.name) LIKE UPPER(%s) 
+        OR LOWER(recipes_ingredient.name) like %s)""", [query, query])
+    context = {
+        'recipes': recipes
+    }
+    return render(request, 'recipes/index.html', context)
