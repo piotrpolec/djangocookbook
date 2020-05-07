42a39d3a7144def2134081e735765a3b967b2ee3
Davenury <57959794+Davenury@users.noreply.github.com>
2020-05-07 18:09:48 +0200

    Add recipe to database

---
+++ recipes/recipes/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('search', views.search, name = 'search'),
+    path('add_recipe', views.add_recipe, name = 'add_recipe'),
+    path('add_recipe_form', views.add_recipe_form_sub, name = 'add_recipe_form'),
+    path('<int:recipe_id>/', views.detail, name = 'detail'),
+    path('categories', views.categories, name = "categories"),
+    path('category/<int:category_id>/', views.category, name = "category"),
+]
